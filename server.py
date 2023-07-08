dc7125bf1082a605f95d403175c11ea4c101d253
Fletcher Porter <me@fletcherporter.com>
2023-07-08 15:33:48 +0300

    Made a tutorial Flask server to try out Heroku

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/getmsg/", methods=["GET"])
+def respond():
+    name = request.args.get("name", None)
+    print(f"got name {name}")
+
+    response = {}
+    if not name:
+        response["ERROR"] = "no name found, please send a name"
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric"
+    else:
+        response["MESSAGE"] = f"Welcome {name}!"
+
+    return jsonify(response)
+
+
+@app.route("/")
+def index():
+    return "<h1>The server says hello</h1>"
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
